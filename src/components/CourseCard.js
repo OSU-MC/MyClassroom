69d9416f7945d33f8be8e3ba7a5629f6ba9603b6
Elayne Trimble <34491985+METrimble@users.noreply.github.com>
2023-03-14 23:10:46 -0700

    Fixed for redux, cleaned up if statement

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,42 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { Button, Card } from "react-bootstrap";
+import { useDispatch } from 'react-redux';
+import { setCourse } from '../redux/actions';
 
 import { Link } from 'react-router-dom';
 
 function CourseCard (props) {
-
   //variable declarations, using hookstates to initilize
-  const [ editedName, setEditedName ] = useState(props.CourseName)
+  //const [ editedName, setEditedName ] = useState(props.CourseName)
   //const [ editedCRN, setEditedCRN ] = useState(props.CourseCRN)
   //const [ editedInstructors, setEditedInstructors ] = useState(props.CourseInstructor)
-  const [ editedCourse, setEditedCourse ] = useState({})
-  const [ saveChanges, setSaveChanges ] = useState(true)
-
+  //const [ editedCourse, setEditedCourse ] = useState({})
+  //const [ saveChanges, setSaveChanges ] = useState(true)
   // console.log(editedName, editedCRN, editedInstructors)
+  //useEffect(() => {
+  //  console.log( editedCourse)
+  //}, [editedCourse])
+  const dispatch = useDispatch()
 
-  
-  useEffect(() => {
+  async function saveCourseClicked(course){
+    console.log(course)
+    dispatch(setCourse(course))
+  }
 
-
-    console.log( editedCourse)
-    
-
-
-  }, [editedCourse])
-
-
-  if(!props.hidden && !props.isEdit){
+  //if(!props.hidden && !props.isEdit){
     return (
           //Using Card imported from React
           <div> 
             <Card>
             <Card.Header>Course Name: {props.CourseName}</Card.Header>
             <Card.Body>
+              {/*OLD CARD CODE*/}
               {/*<Card.Text> Course CRN: {props.CourseCRN}</Card.Text>
               <Card.Text> Course Instructor: {props.CourseInstructor}</Card.Text>*/}
               {/* <Link to={props.view==="student" ? "/" : "/instructor/edit_course"}> */}
-              
-              
               {/*{props.view==="student" ? <Link to={`/student_live_lecture`}><Button className="hideBtn"> Join Session</Button></Link> : <Button 
               variant="primary"
               className="joinBtn" 
@@ -44,22 +41,38 @@ function CourseCard (props) {
                 props.editToggle(true)
                 props.editCourse(props.CourseNumber)
               }}>
-              
               {props.view==="student" ?  <div></div> : <Button className="hideBtn">Go To Sections</Button>} */}
 
-              {props.view==="student" ? <Link to={`/studentSession`}><Button className="joinBtn">Join Session</Button></Link> : <div></div>}
-              {props.view==="student" ? <Link to={`/singleCourse`}><Button className="hideBtn">View Course</Button></Link> : <Link to={`/singleCourse`}><Button className="hideBtn">View Sections For Course</Button></Link>}
+              {/*Join Button*/}
+              {props.view==="student" ? 
+              <Link to={`/studentSession`}>
+                <Button className="joinBtn">
+                  Join Session
+                </Button>
+              </Link> : 
+              <div></div>}
+
+              {/*View Course Button*/}
+              {props.view==="student" ? 
+              <Link to={`/singleCourse`}>
+                <Button className="hideBtn" onClick={() => {saveCourseClicked(props.currentCourse)}}>
+                  View Course
+                </Button>
+              </Link> : 
+              <Link to={`/singleCourse`}>
+                <Button className="hideBtn" onClick={() => {saveCourseClicked(props.currentCourse)}}>
+                  View Sections For Course
+                </Button>
+              </Link>}
 
             </Card.Body>
           </Card>
         </div>
-        //in the above props.view we are checking to see if "student" is either true or false,
-        //truth case we create div, false case we add a link to instructor lectures.
-      )
-      
-  }
+      );
+  //}
   //creating indivudual "Cards" for each interface with different information about the Course.
-  else if(props.isEdit){
+  /*OLD EDIT TOGGLE CODE*/
+  /*else if(props.isEdit){
     return(
       <>
         <Card>
@@ -75,7 +88,7 @@ function CourseCard (props) {
                 </input></Card.Text>
 
 
-              {/*<Card.Text> Course CRN: 
+              <Card.Text> Course CRN: 
                 <input 
                   type='text' 
                   placeholder={props.CourseCRN}
@@ -93,7 +106,7 @@ function CourseCard (props) {
                   onChange={(event)=> {
                     setEditedInstructors(event.target.value)}}
                   >
-                  </input></Card.Text>*/}
+                  </input></Card.Text>
 
 
               <Button 
@@ -122,7 +135,6 @@ function CourseCard (props) {
           className="joinBtn"
           onClick={() => {
             props.editToggle(false)
-          
           }}
           >Done</Button>
         </>
@@ -134,7 +146,7 @@ function CourseCard (props) {
     return(
       <></>
     )
-  }
+  }*/
 }
 
 export default CourseCard;
--- src/pages/InstructorStudentLanding.js
+++ src/pages/InstructorStudentLanding.js
@@ -3,129 +3,84 @@ import { useState, useEffect } from 'react';
 import CourseCard from '../components/CourseCard'
 import { Button, Card } from "react-bootstrap"
 import courseData from "./data/courseData.json"
+import { useDispatch } from 'react-redux';
 import apiUtil from '../utils/apiUtil'
+import { setCourses } from '../redux/actions';
 
 function InstructorStudentLanding(props) {
-
     // Will implement ability to hide/show once we figure out best way to modify the
     // data in the JSON with backend team
     //const [showHidden, setShowHidden] = useState(false)
 
-    //user information
-    const userId = localStorage.getItem("id") // TODO: store in Redux
-
     //course information
+    const dispatch = useDispatch()
     const [ studentCourses, setStudentCourses] = useState([])
     const [ instructorCourses, setInstructorCourses] = useState([])
-
-
-    const [ editToggle, setEditToggle ] = useState(false)
-    const [ editCourse, setEditCourse ] = useState(false)
+    //const [ editToggle, setEditToggle ] = useState(false)
+    //const [ editCourse, setEditCourse ] = useState(false)
 
     const controller = new AbortController()
 
     useEffect( () => {
-        
             async function populateCourses(){
-                
-                //get the courses for the user
-                const resp = await apiUtil('get', `/courses/`)
-                if (resp.status === 200) {
-                    setStudentCourses(resp.data.studentCourses)
-                    setInstructorCourses(resp.data.teacherCourses)
-                    
+                try{
+                    const response = await apiUtil("get", "courses/");
+                    if (response.status === 200) {
+                        console.log(response.data)
+                        //place the courses in states we can use
+                        setStudentCourses(response.data.studentCourses)
+                        setInstructorCourses(response.data.teacherCourses)
+                        //send the data to the redux so it can be used elsewhere
+                        dispatch(setCourses(response.data.studentCourses, response.data.teacherCourses))
+                    }
+                } catch (e) {
+                    if (e instanceof DOMException) {
+                      console.log("== HTTP request cancelled")
+                    } else {
+                      throw e;
+                    }
                 }
-                else {
-                    console.log(resp.data.error)
-                }
-                
-                //get the role from enrollment 
-
             }
             populateCourses()
 
         }, [])
 
-        //student and teacher
-        if(studentCourses.length > 0 && !editToggle && instructorCourses.length > 0){
-            console.log("how?????")
-            console.log(studentCourses)
-            console.log(instructorCourses)
-            return(
-                <>
-                <div id="student-courses">
-                {
-                    studentCourses.map((studentCourse) => 
-                        <CourseCard CourseNumber={studentCourse.id} CourseName={studentCourse.name} view={"student"} />
-                
-                    )
-                }
-                </div>
-                <div className="bkgrnd">
-                    <div className="buttons">
-                    <Button variant="secondary">Add Course</Button>{' '}
-                        
-                    </div>
-                        <div>
-                        {
-                            instructorCourses.map((instructorCourse) =>  
-                                <CourseCard editToggle={setEditToggle} editCourse={setEditCourse} currentCourse={instructorCourse} CourseNumber={instructorCourse.id} CourseName={instructorCourse.name} hidden={false} view={"landing"} setData={props.setData}/>
-                            )
-                        }
-                        </div>
-                </div>
-                </>
-            )
-        }
+        //return cards for student and teacher courses
+        return(
+            <>
+            {studentCourses.length > 0 ? <div id="student-courses">
+            {
+                studentCourses.map((studentCourse) => 
+                    <CourseCard currentCourse={studentCourse} CourseNumber={studentCourse.id} CourseName={studentCourse.name} view={"student"} />
+            
+                )
+            }
+            </div> : <div></div>}
 
-        //just student
-        else if(studentCourses.length > 0){
-            return(
-                <div id="student-courses">
-                {
-                    studentCourses.map((studentCourse) => 
-                        <CourseCard CourseNumber={studentCourse.id} CourseName={studentCourse.name} view={"student"} />
-                
-                    )
-                }
+            {instructorCourses.length > 0 ? <div className="bkgrnd">
+                <div className="buttons">
+                <Button variant="secondary">Add Course</Button>{' '}
+                    
                 </div>
-            )
-        }
-
-        //just teacher
-        else if(!editToggle && instructorCourses.length > 0){
-            return(
-                <>
-                <div className="bkgrnd">
-                    <div className="buttons">
-                    <Button variant="secondary">Add Course</Button>{' '}
-                        
+                    <div>
+                    {
+                        instructorCourses.map((instructorCourse) =>  
+                            <CourseCard currentCourse={instructorCourse} CourseNumber={instructorCourse.id} CourseName={instructorCourse.name} view={"landing"} setData={props.setData}/>
+                        )
+                    }
                     </div>
-                        <div>
-                        {
-                            instructorCourses.map((instructorCourse) =>  
-                                <CourseCard editToggle={setEditToggle} editCourse={setEditCourse} currentCourse={instructorCourse} CourseNumber={instructorCourse.id} CourseName={instructorCourse.name} hidden={false} view={"landing"} setData={props.setData}/>
-                            )
-                        }
-                        </div>
-                </div>
-                </>
-            )
-        }
-        else if(editToggle){
+            </div> : <div></div>}
+            </>
+        )
+
+        //OLD EDIT TOGGLE CODE
+        /*else if(editToggle){
             return(
-                /*TODO: REPLACE VIEW WITH THE USER TYPE INSTEAD OF HARDCODED*/
                 <>
                 <CourseCard editToggle={setEditToggle} editCourse={setEditCourse} currentCourse={instructorCourses[editCourse-1]} CourseNumber={instructorCourses[editCourse-1].id} CourseName={instructorCourses[editCourse-1].name} hidden={false} view={"landing"} isEdit={true}/>
                 </>
             )
-        }
-        else{
-            return(
-                <>
-                </>
-            )
-        }
+        }*/
 }
 
 export default InstructorStudentLanding;
