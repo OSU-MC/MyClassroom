9e30b5d1d017b3de0ff49930f8503e65e93d1de4
Elayne Trimble <34491985+METrimble@users.noreply.github.com>
2023-03-22 09:00:17 -0700

    Product page styling (#12)
    * styling update
    
    * Update Home.js
    
    added my email.
    
    * button update
    
    * sizing fix
    
    ---------
    
    Co-authored-by: Gaven Robertson <94574064+gavenrobertson@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,33 +2,41 @@ import styled from '@emotion/styled/macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button } from 'react-bootstrap';
 import { faLink } from '@fortawesome/free-solid-svg-icons'
+import { borderRadius, fontFamily, fontStyle, minHeight } from '@mui/system';
 
 function Home() {
     return <>
-        <VerticalContainer>
-            <Title>Classroom Polling</Title>
-            <Subtitle>Oregon State University</Subtitle>
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;0,800;1,700;1,800&display=swap" rel="stylesheet"/>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+    <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,400;0,500;0,600;1,400;1,600&display=swap" rel="stylesheet"/>
+
+        <VerticalContainer style={title}>
+            <Title>MyClassroom</Title>
+            <Subtitle1>Senior Capstone Project</Subtitle1>
+            <Subtitle2>An Open Source Classroom Polling  Web Application</Subtitle2>
         </VerticalContainer>
         <VerticalContainer>
+            <div style={arrowdown}>
+                <div className='arrowdown'/>
+            </div>
+        </VerticalContainer>
+        <VerticalContainer className='test' style={about}>
             <TextBox>
                 { productSummary }
             </TextBox>
             <TextBox>
                 { productUse }
             </TextBox>
+            <div style={usebuttoncontainer}>
+                <Button style={usebutton}>Use Polling Software</Button>
+            </div>
         </VerticalContainer>
-        <VerticalContainer>
-            <Section>Project Contacts</Section>
-            <HorizontalContainer>
-                <Contact>Matthew Hotchkiss: mphotchkiss01@gmail.com</Contact>
-                <Contact>Elayne Trimble: </Contact>
-                <Contact>Gaven Robertson: </Contact>
-                <Contact>Sanjay Ramanathan: </Contact>
-                <Contact>Mitchell Stewart: stewamit@oregonstate.edu</Contact>
-            </HorizontalContainer>
-        </VerticalContainer>
-        <VerticalContainer>
-            <Section>GitHub</Section>
+        <VerticalContainer style={githublinks}>
+            <Section1>GitHub Repo</Section1>
             <HorizontalContainer>
                 <Link href="https://github.com/CS461PollingApplication/my-classroom-fe">
                     Project Frontend: <FontAwesomeIcon icon={faLink} />
@@ -38,17 +46,74 @@ function Home() {
                 </Link>
             </HorizontalContainer>
         </VerticalContainer>
-        <VerticalContainer>
-            <Button>Use Polling Software</Button>
+        <VerticalContainer style={contacts}>
+            <Section2>Project Contacts</Section2>
+            <HorizontalContainer>
+                <Contact>Matthew Hotchkiss: mphotchkiss01@gmail.com</Contact>
+                <Contact>Elayne Trimble: trimblma@oregonstate.edu</Contact>
+                <Contact>Gaven Robertson: robergav@oregonstate.edu</Contact>
+                <Contact>Sanjay Ramanathan: ramanasa@oregonstate.edu</Contact>
+                <Contact>Mitchell Stewart: stewamit@oregonstate.edu</Contact>
+            </HorizontalContainer>
         </VerticalContainer>
+        
     </>
+}
+
+const title = {
+    backgroundColor: "#70a84a",
+    color: "white",
+    paddingTop: "15%",
+    paddingBottom: "5%"
+}
+const usebuttoncontainer = {
+    paddingTop: "2.5%",
+    width: "35%",
+    paddingBottom: "20px"
+}
+const usebutton = {
+    backgroundColor: "#72767d",
+    border: "none",
+    width: "100%",
+    borderRadius: "15px",
+    paddingBottom: "4%",
+    paddingTop: "4%",
+   
+    fontFamily: "'Open Sans', sans-serif",
+    fontSize: "calc(7px + 1.6vw)",
+    fontWeight: "300",
+}
+const arrowdown ={
+    backgroundColor: "#70a84a",
+    minWidth: "40px",
+    minHeight: "40px",
+    paddingBottom: "10px",
+    paddingRight: "30px",
+    paddingLeft: "30px",
+    borderBottomRightRadius: "20px",
+    borderBottomLeftRadius: "20px"
+}
+const about = {
+    textAlign: "center",
+    marginRight: "10%",
+    marginLeft: "10%",
+    marginTop: "20px",
+    marginBottom: "20px"
+}
+const githublinks = {
+    backgroundColor: "#70a84a",
+    color: "white",
+    paddingTop: "2%",
+    paddingBottom: "2%"
+}
+const contacts = {
+    paddingTop: "2%"
 }
 
 const VerticalContainer = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
-    margin: 20px;
 `
 
 const HorizontalContainer = styled.div`
@@ -58,32 +123,56 @@ const HorizontalContainer = styled.div`
 `
 
 const Title = styled.h1`
-
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 800; 
+    font-size: calc(14px + 10vw);
 `
 
-const Subtitle = styled.h4`
-
+const Subtitle1 = styled.h4`
+    font-family: 'Open Sans', sans-serif;
+    padding-top: 2%;
+    font-weight: 600; 
+    font-size: calc(10px + 4vw);
 `
 
-const Section = styled.h3`
-    text-decoration: underline;
+const Subtitle2 = styled.h4`
+    padding-top: 10px;
+    font-family: 'Bitter', serif;
+    font-size: calc(8px + 1.8vw);
+`
+
+const Section1 = styled.h3`
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 500;
+    font-size: calc(10px + 3vw);
+`
+const Section2 = styled.h3`
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 500;
+    font-size: calc(10px + 1.8vw);
 `
 
 const TextBox = styled.div`
     display: flex;
     padding: 20px;
+    font-family: 'Open Sans', sans-serif;
+    font-size: calc(7px + 1.2vw);
+    font-weight: 300;
 `
 
 const Link = styled.a`
     text-decoration: none;
     color: inherit;
     margin: 10px;
+    font-family: 'Open Sans', sans-serif;
+    font-size: calc(7px + 1.2vw);
 `
 
 const Contact = styled.div`
     padding: 10px;
     text-align: center;
-
+    font-family: 'Open Sans', sans-serif;
+    font-size: calc(5px + .5vw);
 `
 
 const productSummary = "The Classroom Polling: Oregon State University project is an instance of the Open Source Polling Software developed as part of Oregon State University's senior capstone project."
