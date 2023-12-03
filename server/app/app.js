32f1b68c92efb018fc41a5120f9e143eb0b7f52d
Justin <fernbaughj@gmail.com>
2023-12-03 14:43:11 -0800

    Add server.js & app.js separation back

--- server/app/app.js
+++ server/app/app.js
@@ -1,19 +1,17 @@
-const db = require("./app/models");
+var express = require("express");
 const cookieParser = require("cookie-parser");
-const { logger, morganMiddleware } = require("./lib/logger");
-const express = require("express");
+const { logger, morganMiddleware } = require("../lib/logger");
 const cors = require("cors");
-const port = process.env.PORT || 3001;
 if (process.env.NODE_ENV == "development") {
 	require("dotenv").config({ override: false });
 }
 
-const app = express();
-
+var app = express();
 app.use(express.json());
 app.use(express.static("public"));
 app.use(morganMiddleware);
 app.use(cookieParser());
+
 // TODO: research and implement better, more secure options
 app.use(
 	cors({
@@ -29,34 +27,21 @@ app.use(
  * top-level router lives in api/index.js.  That's what we include here, and
  * it provides all of the routes.
  */
-try {
-	app.use("/", require("./app/api"));
-	app.use("*", function (req, res, next) {
-		res.status(404).send({
-			error: "The requested resource does not exist",
-		});
-	});
-	// this is our global exception handler function. If an error is thrown and caught, use the next() function with the error to pass it down to here
-	app.use(function (err, req, res, next) {
-		logger.error(err);
-		res.status(500).send({
-			error:
-				"Something unexpected happened. Please try again or contact the admin",
-		});
-	});
-} catch (err) {
-	logger.error("Unable to intiailize routes");
-	logger.error(err);
-}
+app.use("/", require("./api"));
 
-db.sequelize
-	.authenticate()
-	.then(function () {
-		app.listen(port, function () {
-			logger.info(`Server is listening on port: ${port}`);
-		});
-	})
-	.catch((error) => {
-		logger.error(`Unable to connect to sequelize database`);
-		logger.error(error);
+app.use("*", function (req, res, next) {
+	res.status(404).send({
+		error: "The requested resource does not exist",
 	});
+});
+
+// this is our global exception handler function. If an error is thrown and caught, use the next() function with the error to pass it down to here
+app.use(function (err, req, res, next) {
+	logger.error(err);
+	res.status(500).send({
+		error:
+			"Something unexpected happened. Please try again or contact the admin",
+	});
+});
+
+module.exports = app;
