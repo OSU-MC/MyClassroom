e54ea36ca387756b0fffea6d6de2d24f45f06664
Justin <fernbaughj@gmail.com>
2024-02-02 13:38:42 -0800

    Prepare gradebook changes

--- server/app/models/enrollment.js
+++ server/app/models/enrollment.js
@@ -1,114 +1,140 @@
-'use strict'
+"use strict";
 
 module.exports = (sequelize, DataTypes) => {
-    const Enrollment = sequelize.define('Enrollment', {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        role: {
-          // ENUM datatypes are stored and queried more efficiently because MySQL maps the strings to integers on the backside of things
-          type: DataTypes.ENUM('student', 'teacher', 'ta'),
-          allowNull: false,
-          validate: {
-            isIn: [['student', 'teacher', 'ta']],
-            notNull: {
-                msg: "Enrollment role required"
-            }
-          }
-        },
-        courseId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'Courses',
-                key: 'id'
-            },
-            allowNull: true,
-            validate: {
-                roleValidation(courseId) {
-                    if (courseId == null || courseId == undefined) {
-                        // a teacher must have a course
-                        if (this.role === 'teacher') {
-                            throw new Error("Teacher must be enrolled in a course")
-                        }
-                    }
-                    else {
-                        // section cannot also be set
-                        if (!(this.sectionId == null)) {
-                            throw new Error("Enrollment cannot be in a section and a course")
-                        }
+	const Enrollment = sequelize.define(
+		"Enrollment",
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			role: {
+				// ENUM datatypes are stored and queried more efficiently because MySQL maps the strings to integers on the backside of things
+				type: DataTypes.ENUM("student", "teacher", "ta"),
+				allowNull: false,
+				validate: {
+					isIn: [["student", "teacher", "ta"]],
+					notNull: {
+						msg: "Enrollment role required",
+					},
+				},
+			},
+			courseId: {
+				type: DataTypes.INTEGER,
+				references: {
+					model: "Courses",
+					key: "id",
+				},
+				allowNull: true,
+				validate: {
+					roleValidation(courseId) {
+						if (courseId == null || courseId == undefined) {
+							// a teacher must have a course
+							if (this.role === "teacher") {
+								throw new Error("Teacher must be enrolled in a course");
+							}
+						} else {
+							// section cannot also be set
+							if (!(this.sectionId == null)) {
+								throw new Error(
+									"Enrollment cannot be in a section and a course"
+								);
+							}
 
-                        // only a teacher is enrolled in a course
-                        if (!(this.role === 'teacher')) {
-                            throw new Error("Only a teacher can be enrolled at the course level")
-                        }
-                    }
-                }
-            }
-        },
-        userId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'Users',
-                key: 'id'
-            },
-            allowNull: false,
-            validate: {
-                notNull: {
-                    msg: "Enrollment must have a user"
-                }
-            }
-        },
-        sectionId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'Sections',
-                key: 'id'
-            },
-            allowNull: true,
-            validate: {
-                roleValidation(sectionId) {
-                    if (sectionId == null || sectionId == undefined) {
-                        // a non-teacher must have a section
-                        if (!(this.role === 'teacher')) {
-                            throw new Error(`${this.role} must be enrolled in a section`)
-                        }
-                    }
-                    else {
-                        // a teacher cannot be enrolled in a section
-                        if (this.role === 'teacher') {
-                            throw new Error("A teacher cannot be enrolled at the section level")
-                        }
-                    }
-                }
-            }
-        }
-    },
-    {
-        indexes: [
-            // avoids duplicate enrollments
-            {
-                unique: true,
-                fields: ['userId', 'courseId'],
-                name: 'custom_unique_teacher_constraint'
-            },
-            {
-                unique: true,
-                fields: ['userId', 'sectionId'],
-                name: 'custom_unique_student_constraint'
-            }
-        ],
-        timestamps: true
-    })
+							// only a teacher is enrolled in a course
+							if (!(this.role === "teacher")) {
+								throw new Error(
+									"Only a teacher can be enrolled at the course level"
+								);
+							}
+						}
+					},
+				},
+			},
+			userId: {
+				type: DataTypes.INTEGER,
+				references: {
+					model: "Users",
+					key: "id",
+				},
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "Enrollment must have a user",
+					},
+				},
+			},
+			// add grade score to enrollments
+			grade: {
+				type: DataTypes.DOUBLE,
+				allowNull: true,
+				// references: {
+				// 	model: "Responses",
+				// 	key: "score",
+				// },
+				// validate: {
+				// 	min: {
+				// 		args: [0],
+				// 		msg: "score cannot be less than 0",
+				// 	},
+				// 	max: {
+				// 		args: [1],
+				// 		msg: "score cannot be more than 1",
+				// 	},
+				// },
+			},
+			sectionId: {
+				type: DataTypes.INTEGER,
+				references: {
+					model: "Sections",
+					key: "id",
+				},
+				allowNull: true,
+				validate: {
+					roleValidation(sectionId) {
+						if (sectionId == null || sectionId == undefined) {
+							// a non-teacher must have a section
+							if (!(this.role === "teacher")) {
+								throw new Error(`${this.role} must be enrolled in a section`);
+							}
+						} else {
+							// a teacher cannot be enrolled in a section
+							if (this.role === "teacher") {
+								throw new Error(
+									"A teacher cannot be enrolled at the section level"
+								);
+							}
+						}
+					},
+				},
+			},
+		},
+		{
+			indexes: [
+				// avoids duplicate enrollments
+				{
+					unique: true,
+					fields: ["userId", "courseId"],
+					name: "custom_unique_teacher_constraint",
+				},
+				{
+					unique: true,
+					fields: ["userId", "sectionId"],
+					name: "custom_unique_student_constraint",
+				},
+			],
+			timestamps: true,
+		}
+	);
 
-    Enrollment.associate = (models) => {
-        Enrollment.belongsTo(models.Course)
-        Enrollment.belongsTo(models.Section)
-        Enrollment.belongsTo(models.User)
-        Enrollment.hasMany(models.Response)
-    }
+	Enrollment.associate = (models) => {
+		Enrollment.belongsTo(models.Course);
+		Enrollment.belongsTo(models.Section);
+		Enrollment.belongsTo(models.User);
+		Enrollment.hasMany(models.Response);
+	};
 
-    return Enrollment
-}
+	return Enrollment;
+};
--- server/app/services/enrollment_service.js
+++ server/app/services/enrollment_service.js
@@ -1,40 +1,46 @@
-const { extractValidFields, validateAgainstSchema } = require('../../lib/validator')
-const db = require('../models/index')
+const {
+	extractValidFields,
+	validateAgainstSchema,
+} = require("../../lib/validator");
+const db = require("../models/index");
 
 const enrollmentInformationSchema = {
-    id: {required: true},
-    role: {required: true},
-    courseId: {required: false},
-    userId: {required: true},
-    sectionId: {required: false},    
-    User: {required: true}
-}
+	id: { required: true },
+	role: { required: true },
+	courseId: { required: false },
+	userId: { required: true },
+	grade: { required: false },
+	sectionId: { required: false },
+	User: { required: true },
+};
 
 const enrollmentInsertSchema = {
-    role: {required: true},
-    courseId: {required: false},
-    userId: {required: true},
-    sectionId: {required: false}
-}
+	role: { required: true },
+	courseId: { required: false },
+	userId: { required: true },
+	sectionId: { required: false },
+};
 
 exports.extractEnrollmentFields = (body) => {
-    return extractValidFields(body, enrollmentInformationSchema)
-}
+	return extractValidFields(body, enrollmentInformationSchema);
+};
 
 exports.validateEnrollmentCreationRequest = (body) => {
-    return validateAgainstSchema(body, enrollmentInsertSchema)
-}
+	return validateAgainstSchema(body, enrollmentInsertSchema);
+};
 
 exports.checkIfTeacher = async (userId, courseId) => {
-    return await db.Enrollment.findOne({
-        where: { 
-            userId: userId,
-            courseId: courseId,
-            role: 'teacher'
-        }
-    })
-}
+	return await db.Enrollment.findOne({
+		where: {
+			userId: userId,
+			courseId: courseId,
+			role: "teacher",
+		},
+	});
+};
 
 exports.extractArrayEnrollmentFields = (enrollments) => {
-    return enrollments.map(enrollment => extractValidFields(enrollment, enrollmentInformationSchema))
-}
+	return enrollments.map((enrollment) =>
+		extractValidFields(enrollment, enrollmentInformationSchema)
+	);
+};
