c8f20b3b9d8bccb741de9826a24f6e66b5ede2a6
Justin <fernbaughj@gmail.com>
2024-02-12 15:24:06 -0800

    Response model and validator changes

--- server/app/models/response.js
+++ server/app/models/response.js
@@ -1,70 +1,81 @@
-'use strict'
+"use strict";
 
 module.exports = (sequelize, DataTypes) => {
-    const Response = sequelize.define('Response', {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        enrollmentId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'Enrollments',
-                key: 'id'
-            },
-            validate: {
-                notNull: {
-                    msg: 'a response must have an enrollment'
-                }
-            }
-        },
-        questionInLectureId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'QuestionInLectures',
-                key: 'id'
-            },
-            validate: {
-                notNull: {
-                    msg: 'a response must have question in a lecture'
-                }
-            }
-        },
-        score: {
-            type: DataTypes.DOUBLE,
-            allowNull: false,
-            validate: {
-                notNull: {
-                    msg: "a response must have a score"
-                },
-                min: {
-                    args: [0],
-                    msg: "score cannot be less than 0"
-                },
-                max: {
-                    args: [1],
-                    msg: "score cannot be more than 1"
-                }
-            }
-        },
-        // the submission object is what was sent in the POST/PUT request body from the frontend. It should store the answers, for example
-        submission: {
-            type: DataTypes.JSON,
-            defaultValue: {}
-        }
-    },
-    {
-        timestamps: true
-    })
+	const Response = sequelize.define(
+		"Response",
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			enrollmentId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				references: {
+					model: "Enrollments",
+					key: "id",
+				},
+				validate: {
+					notNull: {
+						msg: "a response must have an enrollment",
+					},
+				},
+			},
+			questionInLectureId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				references: {
+					model: "QuestionInLectures",
+					key: "id",
+				},
+				validate: {
+					notNull: {
+						msg: "a response must have question in a lecture",
+					},
+				},
+			},
+			score: {
+				type: DataTypes.DOUBLE,
+				allowNull: false,
+				validate: {
+					notNull: {
+						msg: "a response must have a score",
+					},
+					min: {
+						args: [0],
+						msg: "score cannot be less than 0",
+					},
+					max: {
+						args: [1],
+						msg: "score cannot be more than 1",
+					},
+				},
+			},
+			points: {
+				type: DataTypes.INTEGER,
+				allowNull: true,
+			},
+			totalPoints: {
+				type: DataTypes.INTEGER,
+				allowNull: true,
+			},
+			// the submission object is what was sent in the POST/PUT request body from the frontend. It should store the answers, for example
+			submission: {
+				type: DataTypes.JSON,
+				defaultValue: {},
+			},
+		},
+		{
+			timestamps: true,
+		}
+	);
 
-    Response.associate = (models) => {
-        Response.belongsTo(models.Enrollment)
-        Response.belongsTo(models.QuestionInLecture)
-    }
+	Response.associate = (models) => {
+		Response.belongsTo(models.Enrollment);
+		Response.belongsTo(models.QuestionInLecture);
+	};
 
-    return Response;
-}
+	return Response;
+};
--- server/app/services/response_service.js
+++ server/app/services/response_service.js
@@ -1,24 +1,31 @@
-const { extractValidFields, validateAgainstSchema } = require('../../lib/validator')
+const {
+	extractValidFields,
+	validateAgainstSchema,
+} = require("../../lib/validator");
 
 const responseInformationSchema = {
-    id: {required: true},
-    enrollmentId: {required: true},
-    questionInLectureId: {required: true},
-    score: {required: true},
-    submission: {required: true}
-}
+	id: { required: true },
+	enrollmentId: { required: true },
+	questionInLectureId: { required: true },
+	score: { required: true },
+	points: { required: false },
+	totalPoints: { required: false },
+	submission: { required: true },
+};
 
 const responseInsertSchema = {
-    enrollmentId: {required: true},
-    questionInLectureId: {required: true},
-    score: {required: true},
-    submission: {required: true}
-}
+	enrollmentId: { required: true },
+	questionInLectureId: { required: true },
+	score: { required: true },
+	points: { required: false },
+	totalPoints: { required: false },
+	submission: { required: true },
+};
 
 exports.extractResponseInsertFields = (body) => {
-    return extractValidFields(body, responseInsertSchema)
-}
+	return extractValidFields(body, responseInsertSchema);
+};
 
 exports.extractResponseFields = (body) => {
-    return extractValidFields(body, responseInformationSchema)
-}
+	return extractValidFields(body, responseInformationSchema);
+};
