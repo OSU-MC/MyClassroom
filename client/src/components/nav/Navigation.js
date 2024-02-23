e7ff01ca3700ef1073106629968128b9c70d7885
sirtinyhead <sirtinyhead@gmail.com>
2024-02-22 16:18:24 -0800

    updated Signup navbar and left side text

--- client/src/components/nav/Navigation.js
+++ client/src/components/nav/Navigation.js
@@ -8,7 +8,7 @@ function Navigation(props) {
     const location = useLocation()
     const courseRegex = /\/\d+/ //regular expression: / followed by 1 or more digits
     const inCourse = location.pathname.match(courseRegex)
-    const disableTopNavbar = !location.pathname.match("login") //between this and inCourse, surely theres a better way.
+    const disableTopNavbar = !(location.pathname.match("login") || location.pathname.match("create")) //between this and inCourse, surely theres a better way.
 
     return <>
         {disableTopNavbar && <TopNavbar loggedIn={props.loggedIn}/>}
