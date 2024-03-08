5e96e8cb8b2cead99b0c343e12effd254ab7e234
Elijah Durbin <89661058+Durbin-Elijah@users.noreply.github.com>
2024-03-07 19:32:49 -0800

    added semicolons and a small change to a route (#154)

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,27 @@ import { Route, Routes, Navigate, Outlet } from 'react-router-dom';
 /*
   Page Imports
 */
-import Login from './pages/Login'
+import Login from './pages/Login';
 import Profile from './pages/Profile';
-import Signup from './pages/Signup'
+import Signup from './pages/Signup';
 import ConfirmationCodePasswordRequest from './pages/ConfirmationCodePassword'
 import ResetPasswordForLoginUser from './pages/ResetPassword';
-import Confirm from './pages/Confirm'
-import Landing from './pages/Landing'
-import Course from './pages/Course'
-import Lectures from './pages/Lectures'
-import Lecture from './pages/Lecture'
-import Sections from './pages/Sections'
-import Section from './pages/Section'
-import LectureInSection from './pages/LectureInSection'
-import Questions from './pages/Questions'
-import CreateQuestion from './pages/CreateQuestion'
-import Roster from './pages/Roster'
+import Confirm from './pages/Confirm';
+import Landing from './pages/Landing';
+import Course from './pages/Course';
+import Lectures from './pages/Lectures';
+import Lecture from './pages/Lecture';
+import Sections from './pages/Sections';
+import Section from './pages/Section';
+import LectureInSection from './pages/LectureInSection';
+import Questions from './pages/Questions';
+import CreateQuestion from './pages/CreateQuestion';
+import Roster from './pages/Roster';
 import Enrollments from './pages/Enrollments';
-import SingleCoursePage from './pages/SingleCoursePage'
-import AddCourse from './pages/AddCourse'
-import AddLecture from './pages/AddLecture'
-import Home from './pages/Home'
+import SingleCoursePage from './pages/SingleCoursePage';
+import AddCourse from './pages/AddCourse';
+import AddLecture from './pages/AddLecture';
+import Home from './pages/Home';
 import SingleQuestion from './pages/SingleQuestion';
 import Navigation from './components/nav/Navigation';
 import useAuth from './hooks/useAuth'; 
@@ -42,16 +42,13 @@ function App() {
             - login
             - password reset
           */}
-          <Route path='/home' element={<Home />} />
-          <Route element={loggedIn ? <Navigate to='/' /> : <Outlet />}> {/* It seems like previous implementations tried to redirect to login from within each page. Instead this will redirect to '/' when already logged in*/}
-            <Route path='/login' element={<Login />} />
-            <Route path='/create' element={<Signup />} /> {/* Meant to redirect to login */}
-            <Route path='/reset'
-              element={<ConfirmationCodePasswordRequest />}
-            /> {/* Meant to redirect to login */}
-            <Route path='/reset/password'
-              element={<ResetPasswordForLoginUser />}
-            />
+        <Route element={ <Navigation loggedIn={loggedIn}></Navigation> }>
+          <Route path='/home' element={ <Home/> }/>
+          <Route element={ loggedIn ? <Navigate to='/'/> : <Outlet/>}>
+            <Route path='/login' element={ <Login/> } />
+            <Route path='/create' element={ < Signup /> } /> {/* redirects to landing page if a user is logged in already */}
+            <Route path='/reset' element={ < ConfirmationCodePasswordRequest /> } /> {/* redirects to landing page if a user is logged in already */}
+            <Route path='/reset/password' element={ < ResetPasswordForLoginUser /> } />
           </Route>
 
           {/* Routes requiring user to be logged in */}
