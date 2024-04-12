546ab015b40474c4be0bea28302cd4bf2316cf0f
sirtinyhead <sirtinyhead@gmail.com>
2024-04-11 20:32:09 -0700

    removed the last 2 styled dependencies I could find

--- client/src/components/Notice.js
+++ client/src/components/Notice.js
@@ -1,46 +1,19 @@
-import styled from '@emotion/styled/macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleExclamation, faBell, faCheck } from '@fortawesome/free-solid-svg-icons'
-
-const noticeRed = '#ea5b6a'
-const noticeBlue = '#7bb0d0'
-const noticeGreen = '#77dd77'
+import styles from '../styles/notice.css'
 
 function Notice(props) {
-
-    const NoticeContainer = styled.div`
-        display: flex;
-        flex-direction: row;
-        background-color: ${props.status == "error" ? noticeRed : ( props.status == "success" ? noticeGreen : noticeBlue)};
-        padding: 10px;
-        border-radius: 20px;
-        justify-content: center;
-        margin: 10px;
-        max-width: 450px;
-    `
-
-    return <NoticeWrapper>
-        <NoticeContainer>
-            <NoticeSymbol>
+    return(
+     <div className="noticeWrapper">
+        <div className="noticeContainer">
+            <div className="noticeSymbol">
                 <FontAwesomeIcon icon={props.status == "error" ? faCircleExclamation :( props.status == "success" ? faCheck : faBell )} />
-            </NoticeSymbol>
-            <NoticeMessage>{props.message}</NoticeMessage>
-        </NoticeContainer>
-    </NoticeWrapper>
+            </div>
+            <div className="noticeMessage">{props.message}</div>
+        </div>
+    </div>
+    )
     
 }
-
-const NoticeWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-`
-
-const NoticeSymbol = styled.div`
-    margin-right: 10px;
-`
-
-const NoticeMessage = styled.div`
-    font-weight: 600;
-`
 
 export default Notice
