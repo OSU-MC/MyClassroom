2b1fbe9071e65f489551c9763275b2fe0774e14b
Justin <fernbaughj@gmail.com>
2024-04-18 16:03:15 -0700

    Add totalPoints and points to database migration

--- server/db/migrations/20230117193123-create-responses.js
+++ server/db/migrations/20230117193123-create-responses.js
@@ -1,38 +1,46 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('Responses', {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      score: {
-        type: Sequelize.DOUBLE,
-        allowNull: false,
-      },
-      submission: {
-        type: Sequelize.JSON,
-        defaultValue: {}
-      },
-      // these columns should be standardized across all future tables created
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    });
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("Responses", {
+			id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			score: {
+				type: Sequelize.DOUBLE,
+				allowNull: false,
+			},
+			points: {
+				type: Sequelize.INTEGER,
+				allowNull: true,
+			},
+			totalPoints: {
+				type: Sequelize.INTEGER,
+				allowNull: true,
+			},
+			submission: {
+				type: Sequelize.JSON,
+				defaultValue: {},
+			},
+			// these columns should be standardized across all future tables created
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+			},
+			updatedAt: {
+				type: Sequelize.DATE,
+				allowNull: false,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+			},
+		});
+	},
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.dropTable('Responses');
-  }
+	async down(queryInterface, Sequelize) {
+		return queryInterface.dropTable("Responses");
+	},
 };
