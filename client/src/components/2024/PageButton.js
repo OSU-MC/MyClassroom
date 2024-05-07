6219ea9ee3ff3f4cf322c37c6bcfc44ebb121bf4
Elijah <durbine@oregonstate.edu>
2024-05-07 16:32:49 -0700

    Moved over changes from previous branch, adds a button component that can display other components/pages within a modal.

---
+++ client/src/components/2024/PageButton.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styles from '../../styles/PageButton.css'
+
+/**********************************************************************************************************************
+    --PageButton Component--
+
+	This is a component that adds a button to the DOM that when clicked, loads up another page inside a centered modal.
+		Requires proper css styling to work properly.
+			--I believe this will be implemented in a way that the css is loaded wheneber this component is used
+
+    PROPS:
+    	className: as expected with HTML elements, provides a class for css styling the button
+    	children: as expected with HTML elements, provides children for the added button
+    	newPage: The page (as a react component) to load inside the modal
+
+
+**********************************************************************************************************************/
+
+export default class PageButton extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            viewForm: false
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick()
+    {
+
+        this.setState({
+            viewForm: !this.state.viewForm
+        })
+    }
+
+    render(){
+        return(
+            <>
+                <button onClick={this.handleClick} className={this.props.className}>{this.props.children}</button>
+
+                {this.state.viewForm &&
+                <>
+                    <div className='backdrop' />
+                    <div className='centeredModal'>
+                        <div className='modalContents'>
+                            <button onClick={this.handleClick} className='xButton'>X</button>
+                            {this.props.newPage}
+                        </div>
+                    </div>
+                </>
+                }
+                
+            </>
+        )
+    }
+}
