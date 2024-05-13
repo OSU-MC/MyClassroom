5e2ef8f364a2c17ba434ad162ab1c796bb299e50
Elijah <durbine@oregonstate.edu>
2024-05-13 13:42:26 -0700

    removed commented code from previous PR

--- client/src/pages/TeacherLanding.js
+++ client/src/pages/TeacherLanding.js
@@ -18,13 +18,7 @@ function TeacherLanding(props) {
             {/*No Courses*/}
             {message ? <Notice error={error ? "error" : ""} message={message} /> : (!courses.studentCourses && !courses.instructorCourses) ? <Notice message={"You do not have any courses yet"} /> : <></>}
 
-            {/*Teacher Courses*/}
             {courses.teacherCourses && <div id="teacher-courses">
-                    {/*<Link id="create-course-btn" to={`/createcourse`}>
-                        <Button variant="primary" className='btn-add'>Create Course</Button>
-                    </Link> */}
-
-
 
                 <p id="landing-subtitle">Instructor Courses</p>
                 <hr></hr>
