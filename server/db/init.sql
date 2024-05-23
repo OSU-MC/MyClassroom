98fef973ccbd3c3d336a10e531d375ae04e49119
Justin <fernbaughj@gmail.com>
2024-05-23 16:31:23 -0700

    Remove mysql_native_password option in init.sql in favor of configuration option.

--- server/db/init.sql
+++ server/db/init.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE IF NOT EXISTS myclassroom_development;
-CREATE USER IF NOT EXISTS 'dev_admin'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE USER IF NOT EXISTS 'dev_admin'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON myclassroom_development.* TO 'dev_admin'@'%';
 FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS myclassroom_test;
-CREATE USER IF NOT EXISTS 'test_admin'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE USER IF NOT EXISTS 'test_admin'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON myclassroom_test.* TO 'test_admin'@'%';
 FLUSH PRIVILEGES;
